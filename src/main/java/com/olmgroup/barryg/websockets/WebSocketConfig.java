be3d9be7bf6256985bf9239e295a11420aea4c8b
Barry Gallagher <gallagher_barry@yahoo.co.uk>
2017-06-19 23:52:22 +0100

    First commit

---
+++ src/main/java/com/olmgroup/barryg/websockets/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.olmgroup.barryg.websockets;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer{
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config){
+		// TODO can replace this by connecting to ActiveMQ broker
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+	
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/gs-guide-websocket").withSockJS();
+		
+	}
+	
+	
+
+}
